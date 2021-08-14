0826cea3a0136aa826cbc25b31d1c5240c687c59
yasuwo <y.honjo.i@gmail.com>
2021-08-15 01:42:52 +0900

    js and css

--- tsp/js/tsp.js
+++ tsp/js/tsp.js
@@ -1,12 +1,9 @@
-
-
 let p = [];
 let p_best = [];
 let points = [];
 let NUM = 30;
 let H = 320;
 let W = 640;
-let sum_dist = 0;
 let cost = 0;
 let best = 0;
 let epoch = 0;
@@ -20,6 +17,7 @@ let interval;
 let _onplay = true;
 let DIST;
 
+
 function randint(a, b) {
     return a + (Math.floor(Math.random() * 1e9)) % (b - a + 1);
 }
@@ -27,15 +25,15 @@ function randint(a, b) {
 function draw_point(x, y, context, color = 'black') {
     context.fillStyle = color;
     context.beginPath();
-    context.arc(x, y, 6, 0, 2 * Math.PI, true);
+    context.arc(Math.floor(x * W), Math.floor(y * H), 6, 0, 2 * Math.PI, true);
     context.fill();
 }
 
 function draw_line(x, y, z, w, context, color = 'black') {
     context.strokeStyle = color;
     context.beginPath();
-    context.moveTo(x, y);
-    context.lineTo(z, w);
+    context.moveTo(Math.floor(x * W), Math.floor(y * H));
+    context.lineTo(Math.floor(z * W), Math.floor(w * H));
     context.stroke();
 }
 
@@ -87,11 +85,10 @@ function double_bridge() {
     return res;
 }
 
-function hightlight(p, i, j, context, isswap = true) {
-    let color = '#FF0000';
+function hightlight(p, i, j, context) {
+    const color = '#FF0000';
     draw_point(points[p[i]].x, points[p[i]].y, context, color);
     draw_point(points[p[j]].x, points[p[j]].y, context, color);
-    // if(isswap) draw_line(points[p[i]].x, points[p[i]].y, points[p[j]].x, points[p[j]].y, context, color);
 }
 
 function draw_tsp(p, context, color = "black") {
@@ -102,10 +99,10 @@ function draw_tsp(p, context, color = "black") {
     }
 }
 
-function update_info(epoch, cost, best) {
-    document.getElementById('epoch').innerHTML = 'epoch : ' + epoch;
-    document.getElementById('cost').innerHTML = 'cost : ' + Math.floor(cost);
-    document.getElementById('best').innerHTML = 'best : ' + Math.floor(best);
+function update_info(obj) {
+    document.getElementById('epoch').innerHTML = 'epoch : ' + obj.epoch;
+    document.getElementById('cost').innerHTML = 'cost : ' + obj.cost;
+    document.getElementById('best').innerHTML = 'best : ' + obj.best;
 }
 
 function calc_diff_two_opt(i, j) {
@@ -140,26 +137,24 @@ function sleep(interval) {
     return new Promise((resolve) => setTimeout(resolve, interval));
 }
 
-const intervalRepeater = async (callback, interval) => {
-    while (true) {
-        const startTime = Date.now(); // 時間計測用
-        console.log('処理を始めるよ！')
-
-        // 本処理と sleep を同時実行して最低間隔を確保する
+async function intervalRepeater(callback, interval) {
+    while (_onplay) {
         await Promise.all([callback(), sleep(interval)])
-
-        console.log('処理が終わったよ！ 経過時間[ms]:', Date.now() - startTime)
     }
+    return sleep(10);
 }
 
 async function tsp_heuristic() {
     let p_pre = p.concat();
-    let diff = double_bridge();
+    let diff = (randint(0, 60) == 0) ? 0 : double_bridge();
     let best_d = cost;
-    while (true) {
+    while (_onplay) {
         update = false;
         for (let i = 0; i < NUM; i++) {
-            for (let j = 0; j < NUM; j++) {
+            for (let j = i + 1; j < NUM; j++) {
+                if (!_onplay) {
+                    return sleep(10);
+                }
                 let d = calc_diff_two_opt(i, j);
                 if (d < 0) {
                     update = true;
@@ -170,8 +165,8 @@ async function tsp_heuristic() {
                     // p_best = p.concat();
                     // draw_tsp(p, ctx_best, '#0000FF');
                     best_d = Math.min(best_d, cost + diff);
-                    update_info(epoch, cost + diff, best_d);
-                    await sleep();
+                    update_info({ epoch: epoch, cost: cost + diff, best: best_d });
+                    await sleep(10);
                 }
             }
         }
@@ -184,49 +179,48 @@ async function tsp_heuristic() {
         if (cost < best) {
             best = cost;
             p_best = p.concat();
+            document.getElementById('output').value = p_best;
         }
     }
     else {
-        p = p_pre;
+        p = p_pre.concat();
     }
     epoch++;
     draw_tsp(p, ctx);
     draw_tsp(p_best, ctx_best, '#0000FF');
-    update_info(epoch, cost, best);
-    await sleep();
+    update_info({ epoch: epoch, cost: cost, best: best });
+    return sleep(10);
 };
 
-window.onload = function () {
-    canvas = document.getElementById('tsp-canvas');
-    ctx = canvas.getContext('2d');
-    canvas_best = document.getElementById('tsp-canvas-best');
-    ctx_best = canvas_best.getContext('2d');
-    NUM = Number(document.getElementById('number').value);
+
+
+async function clickNumber() {
+    // document.getElementById('stop').removeEventListener('click', _stop, true);
+    // document.getElementById('play').removeEventListener('click', _play, true);
+    await _stop();
+    init();
+    resize();
     reload();
 }
 
-function clickNumber() {
-    document.getElementById('stop').removeEventListener('click', _stop, true);
-    document.getElementById('play').removeEventListener('click', _play, true);
-    // document.getElementById('best').removeEventListener('click', _best, true);
-    reload();
-}
+let intr;
 
-function _stop() {
+async function _stop() {
     if (_onplay) {
-        // clearInterval(interval);
         _onplay = false;
+        await intr;
     }
 }
 
-function _play() {
+async function _play() {
     if (!_onplay) {
-        // interval = setInterval(tsp_heuristic, 10 * NUM);
+        await intr;
         _onplay = true;
+        intr = intervalRepeater(tsp_heuristic, 10);
     }
 }
 
-function reload() {
+function init() {
     epoch = 0;
     p = [];
     vers = [];
@@ -236,7 +230,7 @@ function reload() {
     for (let i = 0; i < NUM; i++) {
         p.push(i);
         vers.push(i);
-        points.push({ x: randint(0, W), y: randint(0, H) });
+        points.push({ x: Math.random(), y: Math.random() });
     }
     DIST = new Array(NUM);
 
@@ -246,29 +240,60 @@ function reload() {
             DIST[i][j] = dist(points[i].x, points[i].y, points[j].x, points[j].y);
         }
     }
+    document.getElementById('input').value = NUM + "\n";
+    for (const elem of points) {
+        document.getElementById('input').value += elem.x + " " + elem.y + "\n";
+    }
 
     p_best = p.concat();
     cost = best = calc_dist();
+    _onplay = true;
+}
+
+function reload() {
+    intr = intervalRepeater(tsp_heuristic, 10);
+}
+
+// インターバル処理が重複しないようにasync/awaitを利用．
+// https://qiita.com/asa-taka/items/888bc5a1d7f30ee7eda2
+
+// main()に相当．
+// ベージが読み込まれてから処理
+window.onload = function () {
+    canvas = document.getElementById('tsp-canvas');
+    ctx = canvas.getContext('2d');
+    canvas_best = document.getElementById('tsp-canvas-best');
+    ctx_best = canvas_best.getContext('2d');
+    NUM = Number(document.getElementById('number').value);
+    // document.getElementById('stop').addEventListener('click', _stop, true);
+    // document.getElementById('play').addEventListener('click', _play, true);
+
+    init();
+    resize();
+    reload();
+}
+
+window.onresize = function () {
+    resize();
+}
+
+
+function resize() {
+    // canvas.height = innerHeight;
+    W = document.getElementsByClassName('container')[0].offsetWidth;
+    W /= 2;
+    // console.log(W);
+    let cws = document.getElementsByClassName('canvas-wrapper');
+    // console.log(cws);
+    for (const div of cws) {
+        div.style.width = W + "px";
+    }
+    canvas.width = W;
+    canvas_best.width = W;
+    H = canvas.height;
     ctx.clearRect(0, 0, W, H);
     ctx_best.clearRect(0, 0, W, H);
 
     draw_tsp(p, ctx);
     draw_tsp(p_best, ctx_best, '#0000FF');
-
-    // if (_onplay) {
-    //     clearInterval(interval);
-    // }
-
-    // interval = setInterval(tsp_heuristic, 10 * NUM);
-    document.getElementById('stop').addEventListener('click', _stop, true);
-    document.getElementById('play').addEventListener('click', _play, true);
-
-    _onplay = true;
-    intervalRepeater(tsp_heuristic, 10);
-    // while (_onplay) {
-    //     tsp_heuristic();
-    // }
-    // document.getElementById('best').addEventListener('click', _best, true);
 }
-
-// https://qiita.com/asa-taka/items/888bc5a1d7f30ee7eda2
