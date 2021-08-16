a14005e45219096f1c98dfcaf9a93e871e1ca3e0
yasuwo <y.honjo.i@gmail.com>
2021-08-16 22:19:54 +0900

    debug css

--- debug/js/div.js
+++ debug/js/div.js
@@ -1,7 +1,8 @@
 // git.dev
 javascript: if (window.location.host == "github.com") { window.open(window.location.href.replace('github.com', 'github.dev'), '_blank'); }
 // show border
-javascript: (function dfs(v, d) { Object.values(v.children).map(e => { e.style.border = 'solid 1px rgba(' + (d * 25) % 255 + ',0,0,' + d / 10 + ')'; dfs(e, d + 1); }); })(document.body, 0);
+javascript: (function dfs(v, d) { Object.values(v.children).map(e => { e.style.border = `solid 1px rgba( ${(d * 25) % 255} ,0,0,${d / 10})`; dfs(e, Math.min(10, d + 1)); }); })(document.body, 0);
+javascript: (function dfs(v) { Object.values(v.children).map(e => { if (e.hasChildNodes) dfs(e); e.style.background = `rgba(128,128,128,0.1)`; }); })(document.body);
 // show tags
 javascript: (function dfs(v) { Object.values(v.children).map(e => { if (e.hasChildNodes) dfs(e); e.innerHTML = '<span style="position:absolute;color:white;background:rgba(0,0,128,0.4)">&lt' + e.tagName.toLowerCase() + '&gt</span>' + e.innerHTML + '<span style="position: absolute;color:white;background:rgba(0,0,128,0.4)">&lt/' + e.tagName.toLowerCase() + '&gt</span>'; }); })(document.body);
 javascript: (function dfs(v) { Object.values(v.children).map(e => { if (e.hasChildNodes) dfs(e); e.innerHTML = '<span style="color:white;background:rgba(0,0,128,0.4)">&lt' + e.tagName.toLowerCase() + '&gt</span>' + e.innerHTML + '<span style="color:white;background:rgba(0,0,128,0.4)">&lt/' + e.tagName.toLowerCase() + '&gt</span>'; }); })(document.body);
